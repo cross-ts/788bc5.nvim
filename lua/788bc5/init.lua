6e5716241c9b90b651b7e829271224e423e353ce
cross-ts <ts.kouhei@gmail.com>
2022-09-22 02:32:22 +0900

    use highlights

--- lua/788bc5/init.lua
+++ lua/788bc5/init.lua
@@ -6,7 +6,7 @@ function M.load()
   end
   vim.g.colors_name = "788bc5"
 
-  for group, parameters in pairs(require("788bc5.theme")) do
+  for group, parameters in pairs(require("788bc5.theme").highlights) do
     vim.api.nvim_set_hl(0, group, parameters)
   end
 end
