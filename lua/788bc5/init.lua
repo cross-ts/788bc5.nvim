9b769ad6a93c79c93581cea0d95b895f5e8cf8ed
cross-ts <ts.kouhei@gmail.com>
2022-09-27 14:53:57 +0900

    Use True Color

--- lua/788bc5/init.lua
+++ lua/788bc5/init.lua
@@ -1,14 +1,19 @@
 local M = {}
 
 function M.load()
+  -- clear other colorscheme
   if vim.g.colors_name then
     vim.cmd("hi clear")
   end
+
   vim.g.colors_name = "788bc5"
 
-  for group, parameters in pairs(require("788bc5.theme").highlights) do
-    vim.api.nvim_set_hl(0, group, parameters)
-  end
+  -- True Color
+  vim.o.termguicolors = true
+
+  -- set highlights
+  local theme = require("788bc5.theme")
+  theme.apply()
 end
 
 return M
--- lua/788bc5/theme.lua
+++ lua/788bc5/theme.lua
@@ -2,8 +2,14 @@ local colors = require("788bc5.colors")
 
 local M = {}
 
+function M.apply()
+  for group, parameters in pairs(M.highlights) do
+    vim.api.nvim_set_hl(0, group, parameters)
+  end
+end
+
 M.highlights = {
-  Comment = { fg = colors.normal.cyan },
+  Comment = { fg = colors.normal.blue },
 }
 
 return M
